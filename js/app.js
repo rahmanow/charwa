a0efa3d6ecaf80ffbae8aa0df0eda48b18e0e76d
sevda kanturk <sevkan1324@gmail.com>
2021-09-16 21:13:00 +0300

    update homepage(responsive)

--- js/app.js
+++ js/app.js
@@ -2,10 +2,38 @@ $(document).ready(function() {
 $(window).scroll(function() {
     let position = $(this).scrollTop();
     console.log(position);
-    if(position >= 1630) {
+    if(position >= 2175) {
       $('.card-list').addClass('fromUp');
     } else {
       $('.card-list').removeClass('fromUp');
     }
   });
 });
+
+
+$(document).ready(function() {
+    $('.client-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+    
+          }
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+      ]
+      }); 
+      });
